79209ed13e59a59b46861eba76cb7ac6b7b57ff7
barrettury <58411313+barrettury@users.noreply.github.com>
2020-04-21 11:35:40 -0500

    debug3
    No errors, but no image

--- Barrett_Portfolio_Site/ShapeMaker.js
+++ Barrett_Portfolio_Site/ShapeMaker.js
@@ -2,9 +2,9 @@ function setup(){
   createCanvas(400,400);
 }
 function draw(){
-  textSize(15)
+  textSize(15);
   background(245);
-  fill(55,150,250)
-  center(5)
-  pent(250,220,65,1)
+  fill(55,150,250);
+  center(5);
+  pent(250,220,65,1);
 }
